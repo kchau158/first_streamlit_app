4f19927f9b21d5a2dcc551eaafc5eca27dc2629d
kchau158 <kchau158@gmail.com>
2023-10-24 16:17:11 -0400

    only show what's selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,9 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #creating a pick list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Apple'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Apple'])
+filter_list = my_fruit_list(fruits_selected)
 
 # display list of items from storage blob
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(filter_list)
 
