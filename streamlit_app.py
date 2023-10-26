bbbf78997c217f7ef3de743ef78a8c5ae32c674e
kchau158 <kchau158@gmail.com>
2023-10-25 20:05:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,18 +23,12 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"watermelon")
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-#streamlit.text(fruityvice_response.json())
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
 #streamlit.dataframe(fruityvice_normalized)
 
-data = [
-    {"id": 1, "name": {"first": "Coleen", "last": "Volk"}},
-    {"name": {"given": "Mark", "family": "Regner"}},
-    {"id": 2, "name": "Faye Raker"},
-]
-stramlit.dataframe(pd.json_normalize(data))
+#streamlit.text(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
 
